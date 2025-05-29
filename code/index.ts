0d7acf37d94f6db936c3ec0554e9c3bd610169dd
Victor <victorsousa.20@outlook.com>
2025-05-29 04:01:57 -0300

    Aula dia 3 - exercicio 2

--- code/index.ts
+++ code/index.ts
@@ -1,7 +1,14 @@
 // Day 3 - Exercise 1
 
-let pi = 3.14159;
-let tau = pi * 2;
+//let pi = 3.14159;
+//let tau = pi * 2;
 
-console.log(`${tau} is ${pi} times two.`);
+//console.log(`${tau} is ${pi} times two.`);
+
+// Day 3 - Exercise 2
+
+let pie: string
+pie = 'blueberry'
+
+console.log(`I like to eat ${pie}-flavored pie.`)
 
