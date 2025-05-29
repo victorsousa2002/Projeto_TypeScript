473bb3e17466b2212ddec7731ec9edb3dad6dfe7
Victor <victorsousa.20@outlook.com>
2025-05-29 04:08:55 -0300

    Aula dia 3 - exercicio 3

--- code/index.ts
+++ code/index.ts
@@ -7,8 +7,15 @@
 
 // Day 3 - Exercise 2
 
-let pie: string
-pie = 'blueberry'
+//let pie: string
+//pie = 'blueberry'
 
-console.log(`I like to eat ${pie}-flavored pie.`)
+//console.log(`I like to eat ${pie}-flavored pie.`)
+
+// Day 3 - Exercise 3
+
+let isDouglas: boolean = true
+
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+
 
--- prod/index.js
+++ prod/index.js
@@ -4,6 +4,9 @@
 //let tau = pi * 2;
 //console.log(`${tau} is ${pi} times two.`);
 // Day 3 - Exercise 2
-let pie;
-pie = 'blueberry';
-console.log(`I like to eat ${pie}-flavored pie.`);
+//let pie: string
+//pie = 'blueberry'
+//console.log(`I like to eat ${pie}-flavored pie.`)
+// Day 3 - Exercise 3
+let isDouglas = true;
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`);
