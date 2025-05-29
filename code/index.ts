8ae32eca4139d1e377f116eb55ac57b56ce04c8d
Victor <victorsousa.20@outlook.com>
2025-05-29 04:17:05 -0300

    Aula dia 3 - exercicio 4

--- code/index.ts
+++ code/index.ts
@@ -14,8 +14,34 @@
 
 // Day 3 - Exercise 3
 
-let isDouglas: boolean = true
+//let isDouglas: boolean = true
 
-console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+//console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
 
+// Day 3 - Exercise 4
 
+const integer: number = 6;
+const float: number = 6.66;
+const hex: number = 0xf00d;
+const binary: number = 0b1010011010;
+const octal: number = 0o744;
+const negZero: number = -0;
+const actuallyNumber: number = NaN;
+const largestNumber: number = Number.MAX_VALUE;
+const mostBiglyNumber: number = Infinity;
+
+const members: number[] = [
+  integer,
+  float,  
+  hex,
+  binary,
+  octal,
+  negZero,
+  actuallyNumber,
+  largestNumber,
+  mostBiglyNumber
+];
+
+members[0] = 12345;
+
+console.log(members);
--- prod/index.js
+++ prod/index.js
@@ -8,5 +8,28 @@
 //pie = 'blueberry'
 //console.log(`I like to eat ${pie}-flavored pie.`)
 // Day 3 - Exercise 3
-let isDouglas = true;
-console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`);
+//let isDouglas: boolean = true
+//console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+// Day 3 - Exercise 4
+const integer = 6;
+const float = 6.66;
+const hex = 0xf00d;
+const binary = 0b1010011010;
+const octal = 0o744;
+const negZero = -0;
+const actuallyNumber = NaN;
+const largestNumber = Number.MAX_VALUE;
+const mostBiglyNumber = Infinity;
+const members = [
+    integer,
+    float,
+    hex,
+    binary,
+    octal,
+    negZero,
+    actuallyNumber,
+    largestNumber,
+    mostBiglyNumber
+];
+members[0] = 12345;
+console.log(members);
